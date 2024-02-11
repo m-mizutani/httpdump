4706534ba81d65e0b37ae982cecf85fb826d6dfa
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2024-02-11 10:01:13 +0900

    Accept any request

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -5,43 +5,35 @@ import (
 	"log/slog"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
 	"github.com/m-mizutani/httpdump/pkg/utils"
 )
 
 type Server struct {
-	mux *chi.Mux
 }
 
 func New() *Server {
-	route := chi.NewRouter()
-	route.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			utils.Logger().Error("Failed to read body", slog.Any("err", err))
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		utils.Logger().Info("HTTP Access",
-			slog.String("method", r.Method),
-			slog.String("path", r.URL.Path),
-			slog.String("query", r.URL.RawQuery),
-			slog.String("remoteAddr", r.RemoteAddr),
-			slog.String("userAgent", r.UserAgent()),
-			slog.String("referer", r.Referer()),
-			slog.Any("headers", r.Header),
-			slog.String("body", string(body)),
-		)
-
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("Hello, World!"))
-	}))
-	return &Server{
-		mux: route,
-	}
+	return &Server{}
 }
 
 func (x *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	x.mux.ServeHTTP(w, r)
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		utils.Logger().Error("Failed to read body", slog.Any("err", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	utils.Logger().Info("HTTP Access",
+		slog.String("method", r.Method),
+		slog.String("path", r.URL.Path),
+		slog.String("query", r.URL.RawQuery),
+		slog.String("remoteAddr", r.RemoteAddr),
+		slog.String("userAgent", r.UserAgent()),
+		slog.String("referer", r.Referer()),
+		slog.Any("headers", r.Header),
+		slog.String("body", string(body)),
+	)
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("Hello, World!"))
 }
